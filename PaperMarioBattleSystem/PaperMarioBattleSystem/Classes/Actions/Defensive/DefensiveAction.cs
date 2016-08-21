01ea74c198709ca72f55ff7cc792fc98640cc338
tdeeb <thomasdeeb1@gmail.com>
2016-08-21 16:31:57 -0700

    Started designing the implementation of Defensive Actions

---
+++ PaperMarioBattleSystem/PaperMarioBattleSystem/Classes/Actions/Defensive/DefensiveAction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PaperMarioBattleSystem.Enumerations;
+
+namespace PaperMarioBattleSystem
+{
+    /// <summary>
+    /// The base class for Defensive Actions BattleEntities can perform in battle.
+    /// <para>A Defensive Action is defined as one that can be done when it's not the entities turn.
+    /// In the first two Paper Mario games, the only Defensive Actions are Guard and Superguard.</para>
+    /// </summary>
+    public abstract class DefensiveAction
+    {
+        protected BattleEntity User = null;
+
+        protected ActionCommand Command { get; set; } = null;
+
+        protected bool CommandEnabled => (Command != null);
+
+        protected float CommandSuccessTimer = 0f;
+        protected float PrevCommandTimer = 0f;
+
+        protected DefensiveAction()
+        {
+            
+        }
+
+        public void Update()
+        {
+            if (CommandEnabled == true)
+            {
+                Command.Update();
+            }
+        }
+    }
+}

fd9e173faf89ae9a0a1b488ab5da059040e20803
tdeeb <thomasdeeb1@gmail.com>
2016-09-08 21:51:08 -0700

    Abstracted out the Action Command code from OffensiveAction
    -Added CommandMoveAction, a base class for MoveActions that require an Action Command
    -Moved Action Command-related code from OffensiveAction into CommandMoveAction and made OffensiveAction derive from it

--- PaperMarioBattleSystem/PaperMarioBattleSystem/Classes/Actions/OffensiveAction.cs
+++ PaperMarioBattleSystem/PaperMarioBattleSystem/Classes/Actions/OffensiveAction.cs
@@ -13,7 +13,7 @@ namespace PaperMarioBattleSystem
     /// <summary>
     /// A BattleAction that can deal damage to BattleEntities
     /// </summary>
-    public abstract class OffensiveAction : MoveAction, ICommandAction
+    public abstract class OffensiveAction : CommandMoveAction
     {
         /// <summary>
         /// The base damage of the action
@@ -40,54 +40,9 @@ namespace PaperMarioBattleSystem
         /// </summary>
         public bool Piercing { get; protected set; } = false;
 
-        /// <summary>
-        /// The ActionCommand associated with the BattleAction
-        /// </summary>
-        public ActionCommand actionCommand { get; set; }
-
-        /// <summary>
-        /// Tells whether the action command is enabled or not.
-        /// Action commands are always disabled for enemies
-        /// </summary>
-        public bool CommandEnabled => (actionCommand != null && User.EntityType != EntityTypes.Enemy && DisableActionCommand == false);
-
-        /// <summary>
-        /// Whether Action Commands are disabled on this action.
-        /// This value automatically resets to false after the action is completed.
-        /// </summary>
-        public bool DisableActionCommand { get; set; }
-
-        /// <summary>
-        /// The result of performing the Action Command
-        /// </summary>
-        protected ActionCommand.CommandResults CommandResult { get; private set; } = ActionCommand.CommandResults.Success;
-
         protected OffensiveAction()
         {
 
-        }
-
-        /// <summary>
-        /// Starts the Action Command's input.
-        /// If the Action Command is not enabled, it will switch to the Failed branch
-        /// </summary>
-        protected void StartActionCommandInput()
-        {
-            if (CommandEnabled == true) actionCommand.StartInput();
-            else OnCommandFailed();
-        }
-
-        protected override void OnStart()
-        {
-            base.OnStart();
-            CommandResult = ActionCommand.CommandResults.Success;
-        }
-
-        protected override void OnEnd()
-        {
-            base.OnEnd();
-            DisableActionCommand = false;
-            CommandResult = ActionCommand.CommandResults.Success;
         }
 
         /// <summary>
@@ -190,64 +145,6 @@ namespace PaperMarioBattleSystem
             int totalDamage = actionDamage + User.BattleStats.Attack;
 
             return totalDamage;
-        }
-
-        //We have OnCommandSuccess() non-virtual so we can perform general functionality for successfully completing any Action Command
-        //Examples include showing the degrees of success ("Nice," "Great," etc.) and increasing the damage dealt by All or Nothing
-        public void OnCommandSuccess()
-        {
-            CommandResult = ActionCommand.CommandResults.Success;
-
-            CommandSuccess();
-        }
-
-        //We have OnCommandFailed() non-virtual so we can perform general functionality for failing to complete any Action Command
-        //Examples include dealing no damage with the All or Nothing Badge
-        public void OnCommandFailed()
-        {
-            CommandResult = ActionCommand.CommandResults.Failure;
-
-            CommandFailed();
-        }
-
-        public abstract void OnCommandResponse(int response);
-
-        protected abstract void CommandSuccess();
-        protected abstract void CommandFailed();
-
-        public override void StartInterruption(Elements element)
-        {
-            if (CommandEnabled == true) actionCommand.EndInput();
-            base.StartInterruption(element);
-        }
-
-        protected override void PreSequenceUpdate()
-        {
-            base.PreSequenceUpdate();    
-
-            //If the action command is enabled, let it handle the sequence
-            if (CommandEnabled == true)
-            {
-                if (actionCommand.AcceptingInput == true)
-                    actionCommand.Update();
-            }
-        }
-
-        public override void Draw()
-        {
-            base.Draw();
-
-            if (InSequence == true)
-            {
-                if (CommandEnabled == true)
-                {
-                    SpriteRenderer.Instance.DrawText(AssetManager.Instance.Font,
-                    $"Command: {Name} performed by {User.Name}",
-                    new Vector2(SpriteRenderer.Instance.WindowCenter.X, 50f), Color.Black, 0f, new Vector2(.5f, .5f), 1.1f, .9f, true);
-
-                    actionCommand?.Draw();
-                }
-            }
         }
     }
 }

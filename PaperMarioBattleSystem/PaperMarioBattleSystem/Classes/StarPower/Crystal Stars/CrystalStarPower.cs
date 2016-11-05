ecac16e4e61b5299a304287cba2050f2196c9e9f
tdeeb <thomasdeeb1@gmail.com>
2016-11-05 02:35:04 -0700

    Research and preliminary start on Star Power
    -Added notes on how Star Power works in the Paper Mario games
    -Added StarPowerBase and CrystalStarPower classes

---
+++ PaperMarioBattleSystem/PaperMarioBattleSystem/Classes/StarPower/Crystal Stars/CrystalStarPower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaperMarioBattleSystem
+{
+    /// <summary>
+    /// Star Power granted by Crystal Stars that can be used by Mario.
+    /// This is the TTYD Star Power. This Star Power is gained from your battle performance, as determined by the Audience.
+    /// </summary>
+    public class CrystalStarPower : StarPowerBase
+    {
+
+    }
+}
---
+++ PaperMarioBattleSystem/PaperMarioBattleSystem/Classes/StarPower/StarPowerBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PaperMarioBattleSystem
+{
+    /// <summary>
+    /// The base class for the two different types of Star Power found in the Paper Mario games (Star Spirits, Crystal Stars).
+    /// </summary>
+    public abstract class StarPowerBase
+    {
+        //Each usable Star Power circle/bar is represented as 100.
+        //So in PM, a full bar of 1 Star Power is 100 and in TTYD one filled circle is 100
+
+        public float StarPowerLevel { get; protected set; } = 0;
+        public float MaxStarPowerLevel { get; protected set; } = 700f;
+    }
+}
